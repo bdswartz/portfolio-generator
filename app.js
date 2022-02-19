4fa660276a6eaf624e8184d26ac10fcf2d68a469
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-02-19 10:08:42 -0500

    completed the projects section

--- app.js
+++ app.js
@@ -133,14 +133,11 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+    
+      console.log('Portfolio complete! Check out index.html to see the output!');
   });
-
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', generatePage(name, github), err => {
-//   if (err) throw new Error(err);
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+});
